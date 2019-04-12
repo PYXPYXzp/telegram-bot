12fc430c3bb8293976aabf3e5639758889eb9c70
Artem Strukov <stryukov.light@gmail.com>
2019-04-12 15:46:16 +0300

    refactor currency.py

--- currency.py
+++ currency.py
@@ -5,88 +5,99 @@ class BaseCurrency:
 
     NAME = ''
 
-    def __init__(self, bid, ask):
+    def __init__(self, bid: float, ask: float):
         self.bid = bid
         self.ask = ask
 
 
 class DollarCurrency(BaseCurrency):
-    NAME = 'USD'   
+    NAME = 'USD'
 
 
 class EuroCurrency(BaseCurrency):
     NAME = 'EUR'
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+class YuanCurrency(BaseCurrency):
+    NAME = 'CNY'
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 class BaseExchangerOrganization:
-    CURRENCIES_CLASSES = [DollarCurrency, EuroCurrency]
-    
-    def __init__(self, data):
-        self.currencies = data.get('currencies')
-        self.currencies_list = []
+    CURRENCIES_CLASSES = [DollarCurrency, EuroCurrency, YuanCurrency]
 
-    def parse_currencies(self):
+    def __init__(self, api_data: dict):
+        self.currencies = api_data.get('currencies')
+        self.currencies_list = self.parse_currencies()
+
+    def parse_currencies(self) -> list:
+        currencies = []
         if self.currencies:
-            for currency in self.CURRENCIES_CLASSES:
-                currency_data = self.currencies.get(currency.NAME)
-                self.currencies_list.append(
-                    currency(bid=currency_data['bid'], ask=currency_data['ask'])
+            for currency_class in self.CURRENCIES_CLASSES:
+                currency_api_data = self.currencies.get(currency_class.NAME)
+                if currency_api_data:
+                    currencies.append(
+                        currency_class(
+                            bid=currency_api_data['bid'],
+                            ask=currency_api_data['ask']
+                        )
                     )
-            return self.currencies_list
+        return currencies
+
 
 class Bank(BaseExchangerOrganization):
-    pass
+    ORGANIZATION_NAME = 'bank'
 
 
 class Exchanger(BaseExchangerOrganization):
-    pass
+    ORGANIZATION_NAME = 'exchanger'
 
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 class BaseCurrencyApi:
 
     API_URL = ''
-    
-    def __init__(self):
-        pass
-    
-    def get_data(self):
+    ORGANIZATION_OPTIONS = {}
+
+    def get_data(self) -> dict:
         return requests.get(self.API_URL).json()
 
-    def parse_data(self, data):
+    def get_currencies_by_organization(self, data: dict):
         pass
 
-    def get_rate_data(self):
+    def get_organizations_data(self):
         data = self.get_data()
-        return self.parse_data(data)
+        return self.get_organization_objects(data)
 
 
 class FinanceAPI(BaseCurrencyApi):
 
     API_URL = 'http://resources.finance.ua/ru/public/currency-cash.json'
-    ORGANIZATION_OPTION = {
+    ORGANIZATION_OPTIONS = {
         1: Bank,
         2: Exchanger
     }
-    
-    def parse_data(self, data):
-        organization_data = data.get('organizations')
-        for x in organization_data:
-            organization_class = self.ORGANIZATION_OPTION[x['orgType']]
-            organization_class(data=x).parse_currencies()
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def get_organization_objects(self, data: dict) -> dict:
+        organization_objects = []
+        organizations_data = data.get('organizations')
+        for org_data in organizations_data:
+            organization_class = self.ORGANIZATION_OPTIONS[org_data['orgType']]
+            organization_objects.append(
+                organization_class(api_data=org_data)
+            )
+        return organization_objects
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 class ExchangeRates:
 
     API_OPTIONS = {
-        'finance': FinanceAPI()
+        'finance': FinanceAPI(),
     }
 
     def __init__(self, **kwargs):
@@ -95,73 +106,56 @@ class ExchangeRates:
         self.time_interval = kwargs.get('time_interval')
         self.api = kwargs.get('api', 'finance')
 
-    def _get_api(self):
+    def _get_api(self) -> object:
         return self.API_OPTIONS[self.api]
 
-    def get_message(self):
+    def get_avg_message(self) -> str:
         api = self._get_api()
-        rate_data = api().get_rate_data()
+        organizations_data = api.get_organizations_data()
+        return self.generate_avg_message(org_data=organizations_data)
 
-        pass
-    
+    def generate_avg_message(self, org_data):
+        exchanges = (
+            f"Менялы EUR покупка {self.get_average_currency(org_data=org_data, currency_name='EUR', exchange_type='exchanger', transaction_type='ask')} "
+            f"продажа {self.get_average_currency(org_data=org_data, currency_name='EUR', exchange_type='exchanger', transaction_type='bid')} "
+            f"USD покупка {self.get_average_currency(org_data=org_data, currency_name='USD', exchange_type='exchanger', transaction_type='ask')} "
+            f"продажа {self.get_average_currency(org_data=org_data, currency_name='USD', exchange_type='exchanger', transaction_type='bid')} "
+            f"CNY покупка {self.get_average_currency(org_data=org_data, currency_name='CNY', exchange_type='exchanger', transaction_type='ask')} "
+            f"продажа {self.get_average_currency(org_data=org_data, currency_name='CNY', exchange_type='exchanger', transaction_type='bid')}"
+        )
+        banks = (
+            f"Банки EUR покупка {self.get_average_currency(org_data=org_data, currency_name='EUR', exchange_type='banks', transaction_type='ask')} "
+            f"продажа {self.get_average_currency(org_data=org_data, currency_name='EUR', exchange_type='banks', transaction_type='bid')} "
+            f"USD покупка {self.get_average_currency(org_data=org_data, currency_name='USD', exchange_type='banks', transaction_type='ask')} "
+            f"продажа {self.get_average_currency(org_data=org_data, currency_name='USD', exchange_type='banks', transaction_type='bid')} "
+            f"CNY покупка {self.get_average_currency(org_data=org_data, currency_name='CNY', exchange_type='banks', transaction_type='ask')} "
+            f"продажа {self.get_average_currency(org_data=org_data, currency_name='CNY', exchange_type='banks', transaction_type='bid')}"
+        )
+        return exchanges + '\n' + banks
+
+    def get_average_currency(
+        self,
+        org_data: list,
+        currency_name: str,
+        exchange_type: str,
+        transaction_type: str,
+    ):
+        default_value = 0.0
+        result_list = []
+        for organization in org_data:
+            if exchange_type == organization.ORGANIZATION_NAME:
+                for currency in organization.currencies_list:
+                    if currency_name == currency.NAME:
+                        result_list.append(
+                            float(getattr(currency, transaction_type))
+                        )
+        if len(result_list):
+            return get_avg_сurrency(result_list)
+        return default_value
+
     def get_graph(self):
         pass
 
-# class Currency:
-#     FINANCE_API_URL = 'http://resources.finance.ua/ru/public/currency-cash.json'
-#     CURRENCY_EXCHANGE = 2
-#     BANKS = 1
-#     USD = 'USD'
-#     EUR = 'EUR'
 
-
-#     def __init__(self):
-#         self.dict_of_currency_in_banks = {self.USD: [], self.EUR: []}
-#         self.dict_of_currency_in_exchanges = {self.USD: [], self.EUR: []}
-#         self.currency_by_organizations_types = {
-#             self.BANKS: self.dict_of_currency_in_banks, 
-#             self.CURRENCY_EXCHANGE: self.dict_of_currency_in_exchanges
-#         }
-    
-#     def get_average_currency(self):
-#         currency_data = self.get_currency_data()
-#         self.parse_currency_data(currency_data)
-#         return self.get_message()
-        
-    
-#     def get_currency_data(self):
-#         return requests.get(self.FINANCE_API_URL).json()
-
-#     def parse_currency_data(self, currency_data):
-#         organizations_currency = currency_data.get('organizations')
-#         if not organizations_currency:
-#             return None
-#         for organization in organizations_currency:
-#             self.add_to_dict_by_organization_type(organization)
-    
-
-#     def add_to_dict_by_organization_type(self, organization):
-#         organization_type = organization.get('orgType')
-#         currencies_USD = organization['currencies'].get(self.USD)
-#         currencies_EUR = organization['currencies'].get(self.EUR)
-#         self.currency_by_organizations_types[organization_type][self.USD].append(currencies_USD)
-#         self.currency_by_organizations_types[organization_type][self.EUR].append(currencies_EUR)
-    
-#     def get_message(self):
-        
-#         exchanges_ask_eur = self.get_avg_сurrency([float(x['ask']) for x in self.currency_by_organizations_types[self.CURRENCY_EXCHANGE][self.EUR] if x])
-#         exchanges_bid_eur = self.get_avg_сurrency([float(x['bid']) for x in self.currency_by_organizations_types[self.CURRENCY_EXCHANGE][self.EUR] if x])
-#         exchanges_ask_usd = self.get_avg_сurrency([float(x['ask']) for x in self.currency_by_organizations_types[self.CURRENCY_EXCHANGE][self.USD] if x])
-#         exchanges_bid_usd = self.get_avg_сurrency([float(x['bid']) for x in self.currency_by_organizations_types[self.CURRENCY_EXCHANGE][self.USD] if x])
-#         banks_ask_eur = self.get_avg_сurrency([float(x['ask']) for x in self.currency_by_organizations_types[self.BANKS][self.EUR] if x])
-#         banks_bid_eur = self.get_avg_сurrency([float(x['bid']) for x in self.currency_by_organizations_types[self.BANKS][self.EUR] if x])
-#         banks_ask_usd = self.get_avg_сurrency([float(x['ask']) for x in self.currency_by_organizations_types[self.BANKS][self.USD] if x])
-#         banks_bid_usd = self.get_avg_сurrency([float(x['bid']) for x in self.currency_by_organizations_types[self.BANKS][self.USD] if x])
-#         exchanges = f'Менялы EUR покупка {exchanges_bid_eur:.2f} продажа {exchanges_ask_eur:.2f}, USD покупка {exchanges_bid_usd:.2f} продажа {exchanges_ask_usd:.2f}' + '\n'
-#         banks = f'Банки EUR покупка {banks_bid_eur:.2f} продажа {banks_ask_eur:.2f}, USD покупка {banks_bid_usd:.2f} продажа {banks_ask_usd:.2f}'
-#         return exchanges + '\n' + banks
-
-    
-#     def get_avg_сurrency(self, currency_list):
-#         return sum(currency_list) / float(len(currency_list))
-
+def get_avg_сurrency(currency_list):
+    return sum(currency_list) / float(len(currency_list))
--- tests.py
+++ tests.py
@@ -3,7 +3,7 @@ import json
 import unittest
 from unittest.mock import patch
 
-from currency import FinanceAPI
+from currency import FinanceAPI, ExchangeRates
 
 
 class TestFinanceAPI(unittest.TestCase):
@@ -15,8 +15,9 @@ class TestFinanceAPI(unittest.TestCase):
     @patch.object(FinanceAPI, 'get_data')
     def test_get_rate_data(self, get_data_mock):
         get_data_mock.return_value = self.api_data
-        finance = FinanceAPI()
-        self.assertIsNotNone(finance.get_rate_data())
+        rates = ExchangeRates()
+        print(rates.get_avg_message())
+        self.assertIsNotNone(rates.get_avg_message())
 
 
 if __name__ == '__main__':
