bba0c87d43aaeb9cb1d7ab8fe972576dc165b21b
artem <winner1040@gmail.com>
2020-11-02 21:34:51 +0200

    fix Dockerfile

--- bot.py
+++ bot.py
@@ -12,7 +12,6 @@ TOKEN = os.environ.get('BOT_TOKEN')
 time = datetime.time(5)
 
 
-
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
